a3a8b878dc12d8dbd7341e7430658cd614a1c012
Chris Wong <cwong@pivotal.io>
2022-05-03 10:49:36 -0400

    Revert "[#181893272]Add Wavefront redirects and dropdown entry"
    This reverts commit 11b3193e34199fea1738475d4a50a1428aed9429.

--- redirects.rb
+++ redirects.rb
@@ -4,5 +4,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # r301 %r{/wavefront-nozzle/(?![\d-]+)(.*)}, '/wavefront-nozzle/3-n/$1'
-r301 %r{/wavefront-nozzle/4-x/(.*)}, "/wavefront-nozzle/3-x/wavefront-docs"
-r301 %r{/wavefront-nozzle/2-x/wavefront-docs}, "/wavefront-nozzle/3-x/wavefront-docs"
+
